d535890d4f07b02cc487290f3cd71d9cd1a06c25
Do Trinh <thanhdo.trinh@gmail.com>
2022-10-17 13:18:43 +0900

    export

--- app/src/main/java/com/dotrinh/fileimport/MainActivity.java
+++ app/src/main/java/com/dotrinh/fileimport/MainActivity.java
@@ -5,12 +5,17 @@ import static com.dotrinh.fileimport.LogUtil.LogI;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.OpenableColumns;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.dotrinh.fileimport.databinding.ActivityMainBinding;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -19,17 +24,39 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    public ActivityMainBinding binding;
     private static final int IMPORT_FILE = 1111;
+    private static String SHARE_FOLDER;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        importPatch();
-        // dirChecker();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.importBtn.setOnClickListener(v -> importPatch());
+        binding.exportBtn.setOnClickListener(v -> shareCommon());
+
+        SHARE_FOLDER = getFilesDir().getPath() + "/share/";
+        dirChecker(SHARE_FOLDER);
+
+        File scanFile = new File(SHARE_FOLDER);
+        if (Objects.requireNonNull(scanFile.list()).length == 0) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    OutputStream outputStream = new FileOutputStream(SHARE_FOLDER + "abc_" + i + ".dotr");
+                    String content = "noi dung " + i;
+                    outputStream.write(content.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     public void importPatch() {
@@ -47,13 +74,13 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == IMPORT_FILE && resultCode == RESULT_OK) {
             ArrayList<Uri> uriArrayList = new ArrayList<>();
-            if (data.getClipData() != null) {
+            if (data.getClipData() != null) {//select multiple file
                 int count = data.getClipData().getItemCount();
                 for (int i = 0; i < count; i++) {
                     Uri uri = data.getClipData().getItemAt(i).getUri();
                     uriArrayList.add(uri);
                 }
-            } else if (data.getData() != null) {
+            } else if (data.getData() != null) {//select single file
                 uriArrayList.add(data.getData());
             }
 
@@ -112,6 +139,36 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return result;
+    }
+
+    public void shareCommon() {
+        File scanFile = new File(SHARE_FOLDER);
+        ArrayList<Uri> uriArr = new ArrayList<>();
+
+        for (String str : Objects.requireNonNull(scanFile.list())) {
+            File srcFile = new File(SHARE_FOLDER + str);
+            Uri uri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", srcFile);
+            uriArr.add(uri);
+            LogI("file da chon: " + srcFile.getAbsolutePath());
+        }
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        Intent intentShareFile = new Intent(Intent.ACTION_SEND_MULTIPLE);
+        intentShareFile.setType("application/octet-stream");
+        intentShareFile.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uriArr);
+        intentShareFile.putExtra(Intent.EXTRA_SUBJECT, "");
+        intentShareFile.putExtra(Intent.EXTRA_TEXT, "");
+        // grant uri permission
+        Intent chooserIntent = Intent.createChooser(intentShareFile, "Export File");
+        List<ResolveInfo> resInfoList = this.getPackageManager().queryIntentActivities(chooserIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolveInfo : resInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            for (Uri item : uriArr) {
+                this.grantUriPermission(packageName, item, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+        }
+        startActivity(chooserIntent);
     }
 
 
